df5ce936bcf9c1027e7caf6c0688edffa11f9dd4
Dula <httpsdula@gmail.com>
2026-05-14 05:40:31 +0300

    debug db connection

--- backend/db.js
+++ backend/db.js
@@ -12,4 +12,12 @@ const pool = new Pool({
     ssl: { rejectUnauthorized: false }
 })
 
+pool.connect((err) => {
+    if (err) {
+      console.error('Database connection error:', err.message)
+    } else {
+      console.log('Database connected successfully!')
+    }
+  })
+  
 module.exports = pool
