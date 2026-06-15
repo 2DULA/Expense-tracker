6eed1f85f3794938bd11377372a863dd7a0b2463
Dula <httpsdula@gmail.com>
2026-06-15 10:57:07 +0300

    fix PORT definition

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,13 @@ const pool = require('./db')
 
 const app = express()
 app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type']
-}))
-app.use(cors({
     origin: 'https://expense-tracker-dula.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type']
 }))
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
 
 // Get all expenses
 app.get('/expenses', async (req, res) => {
